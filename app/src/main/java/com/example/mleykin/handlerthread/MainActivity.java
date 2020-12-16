585b2e42d7ab893263c3b0e5b8c810e0e01780a9
krylovam <krylova.maria.2629@gmail.com>
2020-12-17 01:21:39 +0300

    Initial commit

--- app/src/main/java/com/example/mleykin/handlerthread/MainActivity.java
+++ app/src/main/java/com/example/mleykin/handlerthread/MainActivity.java
@@ -1,62 +1,93 @@
 package com.example.mleykin.handlerthread;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Handler mUiHandler = new Handler();
-    private MyWorkerThread mWorkerThread;
+    ArrayList<String> StoragePaths;
+    private Subscription subscription = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWorkerThread = new MyWorkerThread("myWorkerThread");
-        Runnable task1 = new Runnable() {
-            @Override
-            public void run() {
-                mUiHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this,
-                                "Background task1 is completed",
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
             }
-        };
-        Runnable task2 = new Runnable() {
-            @Override
-            public void run() {
-                mUiHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this,
-                                "Background task2 is completed",
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
+        }
+        else {
+            String[] projection = new String[] {
+                    MediaStore.Images.Media.DATA,
+            };
+
+            Uri ImagesUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            Cursor cur = managedQuery(ImagesUri, projection,"",null,"");
+
+            StoragePaths = new ArrayList<String>();
+            if (cur.moveToFirst()) {
+                int dataColumn = cur.getColumnIndex(MediaStore.Images.Media.DATA);
+
+                do {
+                    StoragePaths.add(cur.getString(dataColumn));
+                } while (cur.moveToNext());
             }
-        };
-        mWorkerThread.start();
-        mWorkerThread.prepareHandler();
-        for (int i=0; i<10; i++) {
-            mWorkerThread.postTask(task1);
-            mWorkerThread.postTask(task2);
+            cur.close();
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        mWorkerThread.quit();
-        super.onDestroy();
+    public void onclickStart(View v) {
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+
+        Collections.shuffle(StoragePaths);
+        Observable<String> observableImagePaths = Observable.from(StoragePaths);
+        subscription = observableImagePaths.subscribeOn(Schedulers.io()).doOnNext(s -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Path -> {
+                    try {
+                        Bitmap currentBitmap = BitmapFactory.decodeFile(Path);
+                        ImageView ivRandom = (ImageView)findViewById(R.id.ivRandom);
+                        ivRandom.setImageBitmap(currentBitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    public void onclickStop(View v) {
+        subscription.unsubscribe();
+        ImageView ivRandom = (ImageView)findViewById(R.id.ivRandom);
+        ivRandom.setImageResource(android.R.color.transparent);
     }
 }
